d9b720a60bf0e98e2d4cf2c35af4ea2432e86d80
bluefly <hisc@swood.co.uk>
2025-02-22 17:33:40 +0000

    goals 2

--- Assets/scripts/cpuScores.cs
+++ Assets/scripts/cpuScores.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class goal : MonoBehaviour
+public class cpuScores : MonoBehaviour
 {
-
+    [SerializeField] int cpuGoals;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cpuGoals = 0;
     }
 
     // Update is called once per frame
@@ -20,6 +20,7 @@ public class goal : MonoBehaviour
     private void onCollisionGoal(Collider other){
         if (other.CompareTag("ball"))
         {
+            cpuGoals += 1;
             Debug.Log("goal");
         }
     }
