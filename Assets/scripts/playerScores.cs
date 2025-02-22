6f7704701a30b4b9aa5f95a0dcd7e29d56b34d1d
AhadPatoary <120006657+AhadPatoary@users.noreply.github.com>
2025-02-22 18:56:39 +0000

    v0.4
    ball physics + ai computer

--- Assets/scripts/playerScores.cs
+++ Assets/scripts/playerScores.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerGoal : MonoBehaviour
+public class playerScores : MonoBehaviour
 {
     [SerializeField] int playerGoals;
     // Start is called before the first frame update
