5d169dab517963db15ae49b29a333924276f4d74
bluefly <hisc@swood.co.uk>
2025-02-23 14:14:30 +0000

    marketplace

--- Assets/Scripts/Marketplace.cs
+++ Assets/Scripts/Marketplace.cs
@@ -6,5 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class Marketplace : MonoBehaviour
 {
-
+    [SerializeField] public Button exitButton;
+    private void Awake()
+    {
+        exitButton = GameObject.Find("exitbutton").GetComponent<Button>();
+        exitButton.interactable = true;
+        exitButton.onClick.AddListener(ExitButton);
+    }
+    public void ExitButton()
+    {
+        exitButton.interactable = false;
+        StartCoroutine(WaitingSceneChange(2.0f, "Pong"));
+    }
+    IEnumerator WaitingSceneChange(float delayTime, string scene) //coroutine to delay the serve
+    {
+        //Wait for the specified delay time before continuing.
+        Debug.Log("changing scene");
+        yield return new WaitForSeconds(delayTime);
+        SceneManager.LoadScene(scene);//serve to the cpu because they conceded
+    }
 }
